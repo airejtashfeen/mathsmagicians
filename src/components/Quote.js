07d1655b3c9bb98249ba30687aaf42fd4b5b0ef6
Airej Tashfeen <airejtashfeen620@gmail.com>
2023-12-23 16:47:07 +0500

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/components/Calculator.css 	new file:   src/components/Header.css 	new file:   src/components/Header.js 	new file:   src/components/Home.css 	new file:   src/components/Home.js 	new file:   src/components/Navbar.css 	new file:   src/components/Navbar.js 	new file:   src/components/Quote.css 	new file:   src/components/Quote.js 	new file:   src/components/calculator.js 	new file:   src/logic/calculate.js 	new file:   src/logic/operate.js

---
+++ src/components/Quote.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './Header';
+import './Quote.css';
+
+const Quote = () => {
+  <Header />;
+  return (
+    <div className="quotePage">
+      <p>
+      Mathematics is, in its own way, the poetry of logical ideas.
+      </p>
+      <big>
+        <b>Albert Einstein</b>
+      </big>
+    </div>
+  );
+};
+
+export default Quote;
